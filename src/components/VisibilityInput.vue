b6d102c7d912b6b26b42df15fd34f6848622bfc7
paretkf <paretk7@gmail.com>
2018-01-18 00:40:57 +0700

    complete issue#7

--- src/components/VisibilityInput.vue
+++ src/components/VisibilityInput.vue
@@ -3,15 +3,15 @@
     <b-field class="is-pulled-right">
       <b-radio-button v-model="visibility"
         native-value="all">
-        <span>All</span>
+        <span>All ( {{ coutAll }} )</span>
       </b-radio-button>
       <b-radio-button v-model="visibility"
         native-value="active">
-        <span>Active</span>
+        <span>Active ( {{coutActive}} )</span>
       </b-radio-button>
       <b-radio-button v-model="visibility"
         native-value="completed">
-        <span>Completed</span>
+        <span>Completed ( {{coutCompleted}} )</span>
       </b-radio-button>
     </b-field>
   </div>
@@ -19,9 +19,32 @@
 
 <script>
 import { store } from '@/store'
+import { mapGetters } from 'vuex'
 
 export default {
   computed: {
+    ...mapGetters(['todos']),
+    coutAll () {
+      if (this.todos) {
+        return this.todos.length
+      } else {
+        return 0
+      }
+    },
+    coutActive () {
+      if (this.todos) {
+        return this.todos.filter(todo => todo.completed === false).length
+      } else {
+        return 0
+      }
+    },
+    coutCompleted () {
+      if (this.todos) {
+        return this.todos.filter(todo => todo.completed === true).length
+      } else {
+        return 0
+      }
+    },
     visibility: {
       get: function () {
         return store.state.visibility
